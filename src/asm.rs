b42bd923238d939ed718668c981d92dcc02214b1
Yi Xie <xieyi318@gmail.com>
2018-11-23 05:30:56 -0500

    Completed calibration code
    Fixed rdtsc, reference http://llvm.org/docs/LangRef.html#inline-assembler-expressions
    Completed calibration code, pretty printed histogram
    Cleaned up suggestions.

--- src/asm.rs
+++ src/asm.rs
@@ -1,33 +1,97 @@
 #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+
+/*Gets processor time*/
+#[inline]
 pub fn get_rdtsc() -> u64 {
     unsafe {
         let rdtsc_val: u64;
-        asm!("mfence");
+        asm!("mfence \n
+              rdtsc  \n
+              mfence \n"
+            :"=A"(rdtsc_val)
+        );
+        rdtsc_val
+    }
+}
+
+/*Accesses location at addresss 'addr'*/
+#[inline]
+pub fn mem_access(addr: *const u64) {
+    unsafe {
         asm!(
-            "rdtsc"
-            :"=r"(rdtsc_val)
-        );
-        asm!("mfence");
-        rdtsc_val << 32 | 0
-    }
-}
-
-pub fn mem_access(addr: *const usize) {
-    unsafe {
-        asm!("movq (%0), %%rax\n"
+        "movq ($0), %rax\n"
         :
-        : "c" (addr)
+        : "r"(addr)
         : "rax"
         );
     }
 }
 
-pub fn flush_cache_line(addr: *const usize) {
+/*Flushed the cache line that contains address 'addr'*/
+#[inline]
+pub fn flush_cache_line(addr: *const u64) {
     unsafe {
-        asm!("clflush 0(%0)\n"
+        asm!("clflush ($0)\n"
         :  
-        : "c" (addr)
+        : "r" (addr)
         : "rax"
         );
     }
+}
+
+/*AT&T Syntax Test*/
+pub fn ret_mem(addr: *const u64) -> u64 {
+    let mut val: u64 = 1000;
+    unsafe {
+        asm!(
+        "movq ($0), $0\n"
+        : "=r"(val)
+        : "r"(addr)
+        : "rax", "rbx"
+        );
+    }
+    val
+}
+
+/*Returns time after a memory access at address 'addr'*/
+pub fn full_reload_time(addr: *const u64) -> u64 {
+    let time: u64;
+    unsafe {
+        asm!("mfence \n
+        lfence \n
+        rdtsc \n
+        lfence \n
+        movl %eax, %esi \n
+        movl ($1), %eax \n
+        lfence \n
+        rdtsc \n
+        subl %esi, %eax \n"
+        :"=A" (time)
+        :"r" (addr)
+        :"%esi", "%eax"
+        );
+    }
+    time
+}
+
+/*Returns time after a memory access and cache line flush, both at address 'addr'*/
+pub fn full_flush_reload_time(addr: *const u64) -> u64 {
+    let time: u64;
+    unsafe {
+        asm!("mfence \n
+        lfence \n
+        rdtsc \n
+        lfence \n
+        movl %eax, %esi \n
+        movl ($1), %eax \n
+        lfence \n
+        rdtsc \n
+        subl %esi, %eax \n
+        clflush ($1) \n"
+        :"=A" (time)
+        :"r" (addr)
+        :"%esi", "%eax"
+        );
+    }
+    time
 }
